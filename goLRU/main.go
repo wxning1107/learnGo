8b187e2ee9f465dc2a6b2fabbe2aa791bab64ff4
wxning <wxningopher@gmail.com>
2020-05-21 16:28:49 +0800

    Merge sort

--- goLRU/main.go
+++ goLRU/main.go
@@ -35,13 +35,17 @@ func (cache *Cache) Put(key string, value interface{}) interface{} {
 	}
 
 	e := &Entry{Key: key, Value: value, next: cache.head}
+
 	if cache.head != nil {
 		cache.head.pre = e
 	}
+
 	cache.head = e
+
 	if cache.tail == nil {
 		cache.tail = e
 	}
+
 	cache.cache[key] = e
 
 	if len(cache.cache) <= cache.capacity {
@@ -74,24 +78,33 @@ func (cache *Cache) moveToHead(e *Entry) {
 		return
 	}
 
+	e.pre.next = e.next
 	if e == cache.tail {
-		cache.tail = e.next
-		e.next.pre = nil
+		cache.tail = e.pre
 	} else {
-		e.pre.next = e.next
 		e.next.pre = e.pre
 	}
 
-	e.pre = cache.head
-	e.next = nil
+	e.pre = nil
+
+	e.next = cache.head
+	cache.head.pre = e
+
 	cache.head = e
 }
 
 func main() {
 	cache := NewCache(2)
-	cache.Put("1", "one")
-	cache.Put("2", "two")
-	cache.Put("3", "three")
-	cache.Put("2", "two")
-	fmt.Println(cache.Get("1"))
+	cache.Put("1", "one")       // 放入元素one，此时one在队列头部
+	fmt.Println(cache.Get("1")) // 此处输出“one”，此时one在队列头部
+	cache.Put("2", "two")       // 放入元素two，此时two在队列头部
+	fmt.Println(cache.Get("1")) // 此处输出“one”，此时one在队列头部
+	cache.Put("3", "three")     // 放入元素three，总元素个数为3，因此最近最少使用的元素“2”会被删除
+	fmt.Println(cache.Get("2")) // 此处输出nil
+	fmt.Println(cache.Get("3")) // 此处输出“three”
+	fmt.Println(cache.Get("3")) // 此处输出“three”
+	fmt.Println(cache.Get("1")) // 此处输出“one”，此时最近最少使用的元素为“3”
+	cache.Put("2", "two")       // 放入元素three，总元素个数为3，因此最近最少使用的元素“3”会被删除
+	fmt.Println(cache.Get("3")) // 此处输出nil
+	fmt.Println(cache.Get("1")) // 此处输出one
 }
