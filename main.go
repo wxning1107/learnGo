7a2413908666ef5cf8dd2c090f108e520e1f1e85
wxning <wxningopher@gmail.com>
2020-06-01 19:03:48 +0800

    imp zkMutex testing

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"math/rand"
-	"time"
 )
 
 type In interface {
@@ -25,10 +22,12 @@ func main() {
 	//t := test{a:b{aa:1}}
 	//err := t.a.start()
 	//fmt.Println(err)
-	tt := b{aa: 1}
-	tt.start()
-	fmt.Println(tt.aa)
-	rand.Seed(time.Now().Unix())
-	fmt.Println(rand.Intn(100))
-	fmt.Println(rand.Intn(100))
+	//tt := b{aa: 1}
+	//tt.start()
+	//fmt.Println(tt.aa)
+	//rand.Seed(time.Now().Unix())
+	//fmt.Println(rand.Intn(100))
+	//fmt.Println(rand.Intn(100))
+	s := make([]int, 0)
+
 }
