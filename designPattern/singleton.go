39a3a9c8c8ed2dcd29bd248bf69d89dd4088a17c
wxning <wxningopher@gmail.com>
2020-07-05 13:24:36 +0800

    singleton pattern

---
+++ designPattern/singleton.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type WebConfig struct {
+	Port int
+}
+
+var wc *WebConfig
+var mu sync.Mutex
+
+func GetConfig() *WebConfig {
+	mu.Lock()
+	defer mu.Unlock()
+	if wc == nil {
+		wc = &WebConfig{Port: 8080}
+	}
+	return wc
+}
+
+var once sync.Once
+
+func InitConfig() *WebConfig {
+	once.Do(func() {
+		wc = &WebConfig{Port: 8000}
+	})
+	return wc
+}
+
+func main() {
+	c1 := GetConfig()
+	c2 := GetConfig()
+	fmt.Println(c1 == c2)
+	wc = nil
+	c3 := InitConfig()
+	c4 := InitConfig()
+	fmt.Println(c3 == c4)
+}
