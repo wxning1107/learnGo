1bff3c9a1b4d5a1a23d4510dae84e10a90574aab
wxning <wxningopher@gmail.com>
2020-08-04 18:06:59 +0800

    standard exporter

--- prometheus/standard/main.go
+++ prometheus/standard/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"learnGoSource/prometheus/standard/utils"
 	"log"
@@ -10,12 +11,13 @@ import (
 )
 
 var (
-	targetUrl = "http://localhost:20193/metrics"
+	targetUrl = "http://localhost:1234/metrics"
 )
 
 func main() {
 	var listenAddress = flag.String("web.listen-address", ":20195", "Address to listen on for web interface and telemetry.")
 	var metricPath = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	flag.Parse()
 
 	fileName := time.Now().Format("2006-01-02") + ".log"
 	err, file := utils.OpenFile(fileName)
@@ -33,10 +35,15 @@ func main() {
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get(targetUrl)
+	fmt.Println(resp.Header)
 	if err != nil {
 		log.Printf("Get %s error: %v", targetUrl, err)
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Wrong atatus code: %d", resp.StatusCode)
+	}
+
 	all, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Printf("Read resp error: %v", err)
