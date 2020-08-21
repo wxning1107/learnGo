1ae7c7746b5ada01926cc962ddc35c931208af3b
wxning <wxningopher@gmail.com>
2020-08-21 18:22:34 +0800

    test

--- test/test1/main.go
+++ test/test1/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 	"time"
 )
 
-func main() {
+func test1() {
 	defer func() {
 		if err := recover(); err != nil {
 			fmt.Printf("recover1: %v", err)
@@ -20,5 +21,21 @@ func main() {
 	time.Sleep(time.Second)
 	err := recover()
 	fmt.Printf("recover2: %v", err)
+}
 
+func test2() {
+	m := map[int]string{}
+	for i := 0; i < 10000; i++ {
+		m[i] = strconv.Itoa(i)
+	}
+	for i := 0; i < 10000; i++ {
+		go func(i int) {
+			//m[i] = strconv.Itoa(i + 1)
+			fmt.Println(m[i])
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
+func main() {
+	test2()
 }
