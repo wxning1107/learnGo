0031f2c315bb66ad306bad687257296bbddf10ac
wxning <wxningopher@gmail.com>
2020-10-15 19:36:19 +0800

    remove testing

--- test/test2/main.go
+++ test/test2/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"learnGoSource/utils"
-	"os"
-)
-
 func main() {
-	s := "qingting"
-	var b bytes.Buffer
-	b.WriteString(s)
-	_, err := b.WriteTo(os.Stdout)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println()
-	src := []string{"b", "c"}
-	dst := utils.AppendFromIndex(src, "a", 0)
-	fmt.Println(dst)
+
 }
