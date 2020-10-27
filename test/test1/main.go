e9145d9060bac3416df3194de79343d7fc9eec3b
wxning <wxningopher@gmail.com>
2020-10-27 15:32:21 +0800

    add k8s deployment yaml detail

--- test/test1/main.go
+++ test/test1/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := new(DescribeServiceResp)
+	//a.Status.LoadBalancerIP = "a"
+	fmt.Println(a.Status.LoadBalancerIP)
+}
+
+type DescribeServiceResp struct {
+	Status ServiceStatus `json:"status"`
+}
+
+type ServiceStatus struct {
+	Endpoints      []ServiceEndpoints `json:"endpoints"`
+	LoadBalancerIP string             `json:"loadBalancer_ip"`
+	ClusterIP      string             `json:"cluster_ip"`
+}
+
+type ServiceEndpoints struct {
+	IP       string `json:"ip"`
+	NodeName string `json:"node_name"`
 }
