e742dd67e1032d2338261b7afb0dd1f00e75dc8f
wxning <wxningopher@gmail.com>
2020-11-02 21:44:36 +0800

    update k8s service yaml detail

--- test/test2/main.go
+++ test/test2/main.go
@@ -3,57 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	// Creates a circuit breaker that will trip if the function fails 10 times
-	//cb := circuit.NewThresholdBreaker(10)
-	//
-	//events := cb.Subscribe()
-	//go func() {
-	//	for {
-	//		e := <-events
-	//		fmt.Println(e)
-	//		// Monitor breaker events like BreakerTripped, BreakerReset, BreakerFail, BreakerReady
-	//	}
-	//}()
-	//
-	//cb.Call(func() error {
-	//	// This is where you'll do some remote call
-	//	// If it fails, return an error
-	//	res, err := http.Get("abc")
-	//	err = errors.New("abc")
-	//	fmt.Printf("resp: %s\n", res)
-	//	return err
-	//
-	//}, 0)
-	//
-	//time.Sleep(time.Second * 3)
-	src := "a"
-	TestString(&src)
-	fmt.Println(src)
-
-	v := 12
-
-	fmt.Println("src:", v)
-
-	change(v)
-
-	fmt.Println("after change:", v)
-
-	p := 12
-
-	changep(&p)
-
-	fmt.Println("after changep:", p)
+	a := new(DescribeCronjobResp)
+	fmt.Println(a.Status.Name)
 }
 
-func TestString(s *string) {
-	a := "abc"
-	*s = a
+type DescribeCronjobResp struct {
+	Status CronjobStatus `json:"status"`
 }
 
-func change(v int) {
-	v = 33
-}
-
-func changep(p *int) {
-	*p = 55
+type CronjobStatus struct {
+	Name             string `json:"name"`
+	LastScheduleTime string `json:"last_schedule_time"`
 }
